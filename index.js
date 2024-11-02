491451b9fb7ad21ccae3af167860bfdedf205a23
Rishwik Mylabathula <rishi.myla2221@gmail.com>
2024-11-02 13:52:48 +0530

    added electronic projects section to main page

---
+++ index.js
@@ -0,0 +1,46 @@
+import { electronicProjectsData } from "./projectsData.js";
+
+/*
+scrolling - section 0 to header 
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    document.querySelector(".section-0").style.padding = "20px";
+    document.querySelector(".name-box-line1").style.opacity = "0";
+    document.querySelector(".name-box-line2").style.display = "none";
+    document.querySelector(".name").style.fontSize = "1.5em";
+  } else {
+    document.querySelector(".section-0").style.padding = "150px";
+    document.querySelector(".name-box-line1").style.opacity = "1";
+    document.querySelector(".name-box-line2").style.display = "block";
+    document.querySelector(".name").style.fontSize = "5em";
+  }
+} */
+
+window.onload = function() {
+    document.querySelector('.name').classList.add('visible');
+};
+
+loadElectronicsProjects();
+function loadElectronicsProjects() {
+    let projectshtml = ``;
+    electronicProjectsData.forEach(project => {
+        projectshtml += `
+            <div class="e-project-container ${project['id-name']}">
+                <div class="project-text-container">
+                    <div class="heading-container">
+                        <h3>${project['title']}</h3>
+                    </div>
+                    <div class="paragraph-container">
+                        <p>${project['description']}</p>
+                    </div>
+                </div>
+                <div class="project-image-container">
+                    <img src=${project['image']}>
+                </div>
+            </div>
+        `;
+        document.querySelector('.electronic-projects').innerHTML = projectshtml;
+    });
+}
---
+++ projectsData.js
@@ -0,0 +1,26 @@
+export const electronicProjectsData = [
+    {
+        "id-name": "self-balacing-robot",
+        "title": "Self-Balancing Robot",
+        "description": "A guided project made in my first year of college. This robot can balance on its own, even when pushed. It can also be controlled via bluetooth with an android application.",
+        "image": "images/self-balancing-robot-image.jpg"
+    },
+    {
+        "id-name": "von-neumann-computer",
+        "title": "Primitive Von-Neumann Computer Simulation",
+        "description": "This is a simulation of a primitive computer with Von-Neumann architecture in Xilinx Vivado software. I studied the architecture and designed the digital circuitry using Vivado Hardware Description Language.",
+        "image": "images/von-neumann-computer-image.jpg"
+    },
+    {
+        "id-name": "cnn-fpga",
+        "title": "CNN on FPGA for Image Recognition",
+        "description": "An implementation of Convolutional Neural Networks (CNN) with parallel processing on FPGA.It was hardcoded to detect an 'X' or an 'O' in an image. This project is a proof of concept for parallel processing capabilities of FPGAs.",
+        "image": "images/cnn-fpga-image.jpg"
+    },
+    {
+        "id-name": "digital-clock",
+        "title": "Digital Clock with ICs",
+        "description": "This project was taken up as a challenge. The challenge is to build a working device using only ICs and components available in an electronics laboratory, from power conversion to implementing logic.",
+        "image": "images/digital-clock-image.jpg"
+    }
+];
